6e86ddd557b446871f587ad1554455e385f43f50
Anirudh Vegesana <anirudh.vegesana@gmail.com>
2019-09-02 20:16:32 -0400

    Change the URL to cam2-crowdsourcing-local.herokuapp.com

--- csgame/settings.py
+++ csgame/settings.py
@@ -96,7 +96,7 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
     'cam2-crowdsourcing.herokuapp.com',
-    'cam2-ece264.herokuapp.com',
+    'cam2-crowdsourcing-local.herokuapp.com',
 ]
 
 
