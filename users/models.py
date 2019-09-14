30f7f3834da0cfa95db45e79ae4493cf8d425f04
Anirudh Vegesana <anirudh.vegesana@gmail.com>
2019-09-14 08:58:37 -0400

    Fix bug in __str__ of ExpertAnswer

--- users/models.py
+++ users/models.py
@@ -227,4 +227,4 @@ class ExpertAnswer(models.Model):
     object=models.CharField(max_length=20)
     answer=models.TextField()
     def __str__(self):
-        return f"{object}: {answer}"
+        return f"{self.object}: {self.answer}"
